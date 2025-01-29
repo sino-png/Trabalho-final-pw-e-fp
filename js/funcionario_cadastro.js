cd823ee5659c8fd7995c9b7bc5a767db4a708347
sino-png <lorena.alves.almeida.lo@gmail.com>
2025-01-29 10:03:50 -0300

    verificarCPF e verificarUsuario

--- js/funcionario_cadastro.js
+++ js/funcionario_cadastro.js
@@ -11,16 +11,57 @@ function cadastrar(){
     
     var funcionario = new Object();
 
-    var user = document.getElementById("usuario").value;
-    var senha = document.getElementById("usuario").value;
-    var cpf = document.getElementById("cpf").value;
-    var sal = parseFloat(document.getElementById("sal").value);
-    var com = parseInt(document.getElementById("com").value);
-    var vendas = 0;
+    funcionario.user = document.getElementById("usuario").value;
+    funcionario.senha = document.getElementById("senha").value;
+    funcionario.cpf = document.getElementById("cpf").value;
+    funcionario.sal = parseFloat(document.getElementById("sal").value);
+    funcionario.com = parseInt(document.getElementById("com").value);
+    funcionario.vendas = 0;
 
-    if (verificarCpf())
+    if (verificarCpf(funcionario.cpf, funcionarios)) {
+      alert ("Este CPF já está cadastrado")
+    }else{
+      if (verificarUsuario(funcionario.user, funcionarios)){
+        alert ("Este nome de usuário já está sendo usado")
+      }else{
+        var posicao = funcionarios.length;
+        funcionarios[posicao] = funcionario;
+
+        localStorage.setItem('funcionarios', JSON.stringify(funcionarios));
+
+        alert ("Cadastro realizado com sucesso <3")
+      }
+    }
     
   }
+}
+
+function verificarCpf(cpf, funcionarios) {
+  var i = 0; 
+  var repete = false;
+  var quantidade = funcionarios.length;
+
+  for (i=0; i < quantidade; i++) {
+    if (cpf == funcionarios[i].cpf) {
+      repete = true
+    }
+  }
+
+  return repete
+}
+
+function verificarUsuario(user, funcionarios) {
+  var i = 0;
+  var repete = false;
+  var quantidade = funcionarios.length;
+
+  for (i=0; i < quantidade; i++) {
+    if (user == funcionarios[i].user) {
+      repete = true
+    }
+  }
+
+  return repete
 }
 
 $(document).ready(function(){
