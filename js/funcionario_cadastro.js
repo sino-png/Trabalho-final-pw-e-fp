bcf3cd165ff705c9ab116b76a6690d1f96b63d4f
sino-png <lorena.alves.almeida.lo@gmail.com>
2025-01-29 08:38:18 -0300

    mascara

--- js/funcionario_cadastro.js
+++ js/funcionario_cadastro.js
@@ -9,8 +9,40 @@ function cadastrar(){
   if ($("#formularioDados").valid()) {
     var funcionarios = JSON.parse(localStorage.getItem('funcionarios'))
     
-    var nome = document.getElementById("nome")
-    var user = document.getElementById("user")
+    var funcionario = new Object();
+
+    var user = document.getElementById("usuario").value;
+    var senha = document.getElementById("usuario").value;
+    var cpf = document.getElementById("cpf").value;
+    var sal = parseFloat(document.getElementById("sal").value);
+    var com = parseInt(document.getElementById("com").value);
+    var vendas = 0;
+
+    if (verificarCpf())
     
   }
 }
+
+$(document).ready(function(){
+    $('#com').mask('00%');
+    $('#cpf').mask('000.000.000-00');
+});
+
+$("#formularioDados").validate({
+  rules: {
+    cpf: {
+      required: true,
+      minlength: 14
+    },
+    com: {
+      maxlength: 3, 
+      required: true
+    },
+    senha: {
+      required: true
+    },
+    nome: {
+      required: true
+    }
+  }
+})
